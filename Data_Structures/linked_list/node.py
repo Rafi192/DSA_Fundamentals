f7396d727c3c17173e81548347eb567141864ac2
Rafi192 <rafihasanmmsc@gmail.com>
2025-11-06 22:09:33 +0600

    Commit of node creatiion in py

---
+++ Data_Structures/linked_list/node.py
@@ -0,0 +1,11 @@
+class Node:
+    def __init__(self, data) -> None:
+        self.data = data  #storing data
+        self.next = None # pointer to the next node
+
+
+#creating the nodes
+
+node1 = Node(10)
+node2 = Node(20)
+node3 = Node(30)
